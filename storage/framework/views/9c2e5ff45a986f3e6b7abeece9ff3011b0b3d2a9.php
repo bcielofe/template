f7fc50b60465ab197e61a27696d41e5fb0edf52a
bcielofe <bcielofe@yahoo.com>
2017-04-26 01:18:35 +0800

    first commit

---
+++ storage/framework/views/9c2e5ff45a986f3e6b7abeece9ff3011b0b3d2a9.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>| Registry</title>
+
+    <!-- Bootstrap -->
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body style="background: url(images/mushrooms.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;">
+
+  <div class="container" style="margin-top: 10%">
+
+    <div class="row" style="background-color: rgba(0,0,0,0.8);">
+      <div class="col-xs-1"></div>
+      <div class="col-xs-10" >
+        <div class="col-sm-7">
+          <img src="images/unnamed.jpg" style="width: 100%; margin-top: 20px; margin-bottom: 20px;">
+        </div>
+        <div class="col-sm-5" style="color: white; margin-top: 50px; margin-bottom: 50px;">
+          <form>
+            <h2>LOGIN</h2>
+            <div class="form-group">
+              <label for="email">Username:</label>
+              <input type="email" class="form-control" id="email">
+            </div>
+            <div class="form-group">
+              <label for="pwd">Password:</label>
+              <input type="password" class="form-control" id="pwd">
+            </div>
+            <button type="submit" class="btn btn-default">Login</button>
+          </form>
+        </div>
+      </div>
+      <div class="col-xs-1"></div>
+    </div>
+    
+  </div>
+
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
